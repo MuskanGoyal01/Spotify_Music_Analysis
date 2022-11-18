666d93d35b2617a50409aa6fd7f5d00aaa55283e
MUSKAN <muskangoyal3477@gmail.com>
2022-11-19 05:08:47 +0530

    Updated code

--- Code.py
+++ Code.py
@@ -1,60 +1,72 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.express as px
 import seaborn as sns
+
+import warnings
+warnings.simplefilter(action='ignore', category = FutureWarning)
 
 # Loading the DataSet
 df = pd.read_csv("Statics/data.csv")
 print(df.head())
+print('\n')
 
 # Data Pre-Processing
 df.drop('Unnamed: 0', axis = 1, inplace=True)
 print(df.head())
+print('\n')
 
 print(df.info())
+print('\n')
 
-#Checking the number of unique values in each column
+# Checking the number of unique values in each column
 dict = {}
 for i in list(df.columns):
     dict[i]=df[i].value_counts().shape[0]
 print(pd.DataFrame(dict, index=['Unique count']).T)
+print('\n')
 
-#Summary Statistics
+# Summary Statistics
 print(df.describe())
-
-# Categorical Columns And Numeric Columns
-cat_cols = df.select_dtypes(exclude="number").columns.to_list()
-print(cat_cols)
-num_cols = df.select_dtypes(include="number").columns.to_list()
-print(num_cols)
+print('\n')
 
 # Classifying data into numerical and categorical variables.
-data_numerical=df[num_cols]
-data_categorical=df[cat_cols]
-print(data_categorical.head())
-print(data_numerical.head())
+cat_cols = df.select_dtypes(exclude="number").columns.to_list()
+print("Categorical Variables:",cat_cols,'\n')
 
-#Correlation Matrix And Plot
+num_cols = df.select_dtypes(include="number").columns.to_list()
+print("Numerical Variables:",num_cols,'\n')
+
+
+data_numerical = df[num_cols]
+data_categorical = df[cat_cols]
+print(data_categorical.head())
+print('\n')
+print(data_numerical.head())
+print('\n')
+
+# Correlation Matrix And Plot
 correlation = data_numerical.corr()
 print(correlation)
+print('\n')
 sns.heatmap(correlation,cmap="Greens_r",annot=True)
 plt.show()
 
+# Liked vs. DisLiked Songs
+labels = ["Liked Songs"," DisLiked Songs"]
+values = df['target'].value_counts().tolist()
+plt.pie(values, labels = labels, autopct= '%.1f%%', shadow=True,startangle = 60)
+plt.title("Liked/DisLiked Songs Distribution Pie Chart")
+plt.show()
+
 # Skewness and kurtosis
-s_k=[]
+s_k = []
 for i in data_numerical.columns:
     s_k.append([i,data_numerical[i].skew(),data_numerical[i].kurt()])
 skew_kurt=pd.DataFrame(s_k,columns=['Columns','Skewness','Kurtosis'])
 print(skew_kurt)
+print('\n')
 
-## Analysis
-# Liked vs. DisLiked Songs
-labels = ["Liked Songs"," DisLiked Songs"]
-values = df['target'].value_counts().tolist()
-fig = px.pie(df, values=values, names=labels, title="Liked/DisLiked Songs Distribution Pie Chart")
-fig.show()
-
-#Numerical Variable Analysis
+# Numerical Variable Analysis
 cols = ['acousticness','danceability','energy','instrumentalness','liveness','speechiness','tempo','loudness','valence']
 fig, axes = plt.subplots(3,3)
 ax_no = 1
@@ -70,7 +82,7 @@ for col in cols:
 fig.suptitle('Kde Plots')
 plt.show()
 
-#Subplots of Top Artists,Loudest Songs,Energetic Songs
+# Subplots of Top Artists, Loudest Songs,Energetic Songs
 fig,ax = plt.subplots(3,1)
 top_artist_count = df.artist.value_counts()
 sns.barplot(x = top_artist_count.values[:6], y= top_artist_count.index[:6], ax = ax[0])
@@ -101,16 +113,7 @@ sns.barplot(y="song_title",x="valence",data=top_10,ax=ax[2])
 ax[2].set_title("Songs with Most Valence")
 plt.show()
 
-#Exploring Continuous Data
-sns.set_palette("Blues_r")
-ax_no = 1
-for col in num_cols:
-    plt.subplot(5, 3, ax_no)
-    sns.histplot(data = df, x=col, bins=25, kde=True)
-    ax_no += 1
-plt.show()
-
-#EDA
+# EDA
 sns.set_palette("viridis_r")
 ax_no = 1
 for col in num_cols:
@@ -119,11 +122,20 @@ for col in num_cols:
     ax_no += 1
 plt.show()
 
+# Exploring Data
+sns.set_palette("Blues_r")
+ax_no = 1
+for col in num_cols:
+    plt.subplot(5, 3, ax_no)
+    sns.histplot(data = df, x=col, bins=25, kde=True)
+    ax_no += 1
+plt.show()
+
 continuous_cols = ['acousticness', 'danceability', 'duration_ms', 'energy',
     'liveness', 'loudness',  'tempo',  'valence', 'speechiness', 'instrumentalness']
 discrete_cols = ['key','mode','time_signature','target']
 
-#Distributions of Continuous Features
+# Distributions of Continuous Features
 fig, axes = plt.subplots(5,2)
 palettes = ['viridis','Set1', 'prism', 'rocket']
 axes = axes.flatten()
@@ -135,7 +147,7 @@ for col in continuous_cols:
 fig.suptitle('Distributions of Continuous Features')
 plt.show()
 
-#Distributions of Discrete Features
+# Distributions of Discrete Features
 sns.set_palette("Set1")
 fig, axes = plt.subplots(2,2)
 palettes = ['tab10', 'Paired', 'rocket', 'Set1']
@@ -148,7 +160,7 @@ for col in discrete_cols:
 fig.suptitle('Distributions of Discrete Features')
 plt.show()
 
-#Correlation Analysis
+# Correlation Analysis
 plt.figure()
 plt.subplot(3,2,1)
 sns.scatterplot(data=df,x=df['acousticness'],y=df['danceability'],hue=df['target'],palette="OrRd",style=df['target'])
